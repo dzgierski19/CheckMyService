4f9cb9ec8691ecdd4f9d4515c40907e07212a0c5
dzgierski19 <dzgierskigit@gmail.com>
2024-02-13 19:26:48 +0100

    schemas for posting and deleting websites

--- src/schemas/ValidateSchema.ts
+++ src/schemas/ValidateSchema.ts
@@ -1,3 +1,4 @@
 import z from "zod";
 
-export const checkWebsiteSchema = z.string().url();
+export const postWebsiteSchema = z.object({ data: z.string().url() });
+export const deleteWebsiteSchema = z.object({ id: z.string().uuid() });
