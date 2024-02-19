20fcce9b31d6958b1353ad2fb9d6f7d26a4fe24f
dzgierski19 <dzgierskigit@gmail.com>
2024-02-19 11:43:29 +0100

    changed schema for post method

--- src/schemas/ValidateSchema.ts
+++ src/schemas/ValidateSchema.ts
@@ -1,4 +1,4 @@
 import z from "zod";
 
-export const postWebsiteSchema = z.object({ data: z.string().url() });
+export const postWebsiteSchema = z.object({ url: z.string().url() });
 export const deleteWebsiteSchema = z.object({ id: z.string().uuid() });
