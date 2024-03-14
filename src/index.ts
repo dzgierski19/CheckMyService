34b1de965b708d31888cae6a0a631cfb899c86d8
dzgierski19 <dzgierskigit@gmail.com>
2024-03-14 12:05:43 +0100

    added error handling middleware and routes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
-import express from "express";
-import { websiteRouter } from "./routes/WebsiteRouter";
+import express, { NextFunction, Request, Response } from "express";
+import { websiteRouter } from "./domains/websites/WebsiteRouter";
+import { logRouter } from "./domains/logs/LogRouter";
+import { validatorMiddleware } from "./middlewares/validationMiddleware";
+import { errorHandler } from "./middlewares/errorHandler";
+import { DomainError, PathError } from "./domains/errors/Errors";
 
-const app = express();
+export const app = express();
 const port = 8500;
 
 app.use(express.json());
+
 app.use(websiteRouter);
+app.use(logRouter);
+
+app.use(errorHandler);
+
+// app.all("*", (req: Request, res: Response, next: NextFunction) => {
+//   next(new PathError(`Can't reach path ${req.path}`));
+// });
+
+// cron.checkWebsites();
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+// errorHandling, cron, routes, zod, dodac validation middleware (ZOD)
