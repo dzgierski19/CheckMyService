e2a37ef25a9d848885f3b24e7a721e18c3a32999
dzgierski19 <dzgierskigit@gmail.com>
2024-03-14 12:08:19 +0100

    schemas to check incoming data

--- src/schemas/ValidateSchema.ts
+++ src/schemas/ValidateSchema.ts
@@ -1,4 +1,32 @@
 import z from "zod";
 
-export const postWebsiteSchema = z.object({ url: z.string().url() });
-export const deleteWebsiteSchema = z.object({ id: z.string().uuid() });
+export const postWebsiteSchema = z
+  .object({
+    body: z.object({ url: z.string().url() }),
+  })
+  .strict();
+
+export const deleteWebsiteSchema = z
+  .object({
+    body: z.object({ id: z.string().uuid() }),
+  })
+  .strict();
+
+export const postLogSchema = z
+  .object({
+    body: z.object({ id: z.string().uuid() }),
+  })
+  .strict();
+
+export const deleteLogSchema = z
+  .object({
+    body: z.object({
+      id: z.string().uuid(),
+    }),
+  })
+  .strict();
+
+export type postWebsiteRequest = z.infer<typeof postWebsiteSchema>;
+export type deleteWebsiteRequest = z.infer<typeof deleteWebsiteSchema>;
+export type postLogRequest = z.infer<typeof postLogSchema>;
+export type deleteLogRequest = z.infer<typeof deleteLogSchema>;
