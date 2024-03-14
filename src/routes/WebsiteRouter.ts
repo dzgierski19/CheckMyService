a52f29a3b2cbe2f48428c854a596e6dc81f9aa22
dzgierski19 <dzgierskigit@gmail.com>
2024-03-14 12:16:13 +0100

    update

--- src/routes/WebsiteRouter.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { websiteController } from "../IoC";
-export const websiteRouter = Router();
-
-websiteRouter.post("/websites", websiteController.postWebsite);
-websiteRouter.delete("/websites/", websiteController.deleteWebsite);
-websiteRouter.get("/websites", websiteController.getWebsites);
-websiteRouter.get("/webs", websiteController.getSomething);
