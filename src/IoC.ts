4f52688b31a6a6cf2ba50e74bfb1ca54c3f5a3e5
dzgierski19 <dzgierskigit@gmail.com>
2024-03-14 12:15:01 +0100

    updated IoC

--- src/IoC.ts
+++ src/IoC.ts
@@ -1,9 +1,19 @@
 import { PrismaClient } from "@prisma/client";
-import { WebsiteController } from "./controllers/WebsiteController";
-import { WebsiteService } from "./domains/services/WebsiteService";
+import { WebsiteService } from "./domains/websites/WebsiteService";
 import { WebsiteRepository } from "./domains/websites/WebsiteRepository";
+import { LogRepository } from "./domains/logs/LogRepository";
+import { LogService } from "./domains/logs/LogService";
+import { WebsiteController } from "./domains/websites/WebsiteController";
+import { LogController } from "./domains/logs/LogController";
+import { Cron } from "./Cron";
 
-const websiteService = new WebsiteService();
-export const websiteRepo = new WebsiteRepository(websiteService);
-export const websiteController = new WebsiteController(websiteRepo);
+export const CRON_EVERY_10_SECONDS = "*/10 * * * * *";
+
 export const prisma = new PrismaClient();
+export const websiteRepo = new WebsiteRepository();
+export const websiteService = new WebsiteService(websiteRepo);
+const logRepository = new LogRepository();
+export const logService = new LogService(logRepository, websiteService);
+export const websiteController = new WebsiteController(websiteService);
+export const logController = new LogController(logService, websiteService);
+export const cron = new Cron(logService, websiteService);
