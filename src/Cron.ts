4666e8a6c1746acfc690bbd858fb1ffebc5d504d
dzgierski19 <dzgierskigit@gmail.com>
2024-03-29 15:03:47 +0100

    updated cron class to use only CSVService

--- src/Cron.ts
+++ src/Cron.ts
@@ -3,25 +3,27 @@ import { IWebsiteRepository } from "./domains/websites/WebsiteRepository";
 import { ILogService, LogService } from "./domains/logs/LogService";
 import { IWebsiteService } from "./domains/websites/WebsiteService";
 import { CRON_EVERY_10_SECONDS, logService, websiteService } from "./IoC";
+import fs, { PathLike } from "fs";
+import { ICSVService } from "./CSVService";
 
 export interface ICron {
-  checkWebsites(): Promise<void>;
+  checkWebsites(path: PathLike): Promise<void>;
 }
 
 export class Cron implements ICron {
   constructor(
-    private readonly logService: ILogService,
-    private readonly websiteService: IWebsiteService
+    // private readonly logService: ILogService,
+    // private readonly websiteService: IWebsiteService,
+    private readonly CSVService: ICSVService
   ) {}
 
-  async checkWebsites() {
+  async checkWebsites(path: PathLike) {
     new CronJob(CRON_EVERY_10_SECONDS, async () => {
-      const websites = await this.websiteService.getAllWebsites();
-      websites.forEach((element) => {
-        if (!element.deletedAt) {
-          this.logService.addLogForWebsite(element.id);
-        }
-      });
+      // const websites = await this.websiteService.getAllWebsites();
+      // websites.forEach((element) => {
+      //   this.logService.addLogForWebsite(element.id);
+      // });
+      await this.CSVService.saveWebsiteLogsToCSV(path);
     }).start();
   }
 }
