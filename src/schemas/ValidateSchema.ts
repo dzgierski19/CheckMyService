2bb6bcdad07483c521ffe3a93786cb12c74d93fe
dzgierski19 <dzgierskigit@gmail.com>
2024-03-29 16:33:37 +0100

    added schemas and its types for new requests

--- src/schemas/ValidateSchema.ts
+++ src/schemas/ValidateSchema.ts
@@ -1,32 +1,55 @@
 import z from "zod";
 
-export const postWebsiteSchema = z
-  .object({
-    body: z.object({ url: z.string().url() }),
-  })
-  .strict();
+export const postWebsiteSchema = z.object({
+  body: z.object({ url: z.string().url() }),
+});
 
-export const deleteWebsiteSchema = z
-  .object({
-    body: z.object({ id: z.string().uuid() }),
-  })
-  .strict();
+export const deleteWebsiteSchema = z.object({
+  body: z.object({ id: z.string().uuid() }),
+});
 
-export const postLogSchema = z
-  .object({
-    body: z.object({ id: z.string().uuid() }),
-  })
-  .strict();
+export const postLogSchema = z.object({
+  body: z.object({ id: z.string().uuid() }),
+});
 
-export const deleteLogSchema = z
-  .object({
-    body: z.object({
-      id: z.string().uuid(),
-    }),
-  })
-  .strict();
+export const deleteLogSchema = z.object({
+  body: z.object({
+    id: z.string().uuid(),
+  }),
+});
+
+const validateParams = z.object({ websiteId: z.string().uuid() });
+const validateQuery = z.object({
+  // start: z.coerce.date(),
+  // finish: z.coerce.date(),
+  start: z.string(),
+  finish: z.string(),
+});
+
+export const getRaportSchema = z.object({
+  params: validateParams,
+  query: validateQuery,
+});
+
+export const websitesPaginationSchema = z.object({
+  query: z.object({
+    page: z.number(),
+    limit: z.number(),
+  }),
+});
+
+export const logPaginationSchema = z.object({
+  params: z.object({ websiteId: z.string().uuid() }),
+  query: z.object({
+    page: z.number(),
+    limit: z.number(),
+  }),
+});
 
 export type postWebsiteRequest = z.infer<typeof postWebsiteSchema>;
 export type deleteWebsiteRequest = z.infer<typeof deleteWebsiteSchema>;
 export type postLogRequest = z.infer<typeof postLogSchema>;
 export type deleteLogRequest = z.infer<typeof deleteLogSchema>;
+export type getRaportRequest = z.infer<typeof getRaportSchema>;
+export type websitePaginationRequest = z.infer<typeof websitesPaginationSchema>;
+export type logPaginationRequest = z.infer<typeof logPaginationSchema>;
