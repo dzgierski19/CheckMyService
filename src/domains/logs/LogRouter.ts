6fbf984b9b0555471a838448388f401ab8bfda8b
dzgierski19 <dzgierskigit@gmail.com>
2024-03-29 16:30:15 +0100

    added validator middleware with schemas to routes

--- src/domains/logs/LogRouter.ts
+++ src/domains/logs/LogRouter.ts
@@ -1,10 +1,19 @@
 import { Router } from "express";
 import { logController } from "../../IoC";
+import { validatorMiddleware } from "../../middlewares/validationMiddleware";
+import { deleteLogSchema, postLogSchema } from "../../schemas/ValidateSchema";
 require("express-async-errors");
 
 export const logRouter = Router();
-
-logRouter.post("/logs", logController.postLog);
+logRouter.post(
+  "/logs",
+  validatorMiddleware(postLogSchema),
+  logController.postLog
+);
 logRouter.get("/websites/:websiteId/logs/", logController.getLogs);
 logRouter.get("/websites/:websiteId/logs/:logId", logController.getLog);
-logRouter.delete("/logs", logController.deleteLog);
+logRouter.delete(
+  "/logs",
+  validatorMiddleware(deleteLogSchema),
+  logController.deleteLog
+);
