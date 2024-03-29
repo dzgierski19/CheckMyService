d854003ce0bbf6f04091a9b6e123085d4cc50bcf
dzgierski19 <dzgierskigit@gmail.com>
2024-03-29 15:02:49 +0100

    updated instance of csvService

--- src/IoC.ts
+++ src/IoC.ts
@@ -6,8 +6,12 @@ import { LogService } from "./domains/logs/LogService";
 import { WebsiteController } from "./domains/websites/WebsiteController";
 import { LogController } from "./domains/logs/LogController";
 import { Cron } from "./Cron";
+import { CSVService } from "./CSVService";
+import path from "path";
 
 export const CRON_EVERY_10_SECONDS = "*/10 * * * * *";
+export const PATH_TO_CRON_CSV = path.join(__dirname, "WebsiteData.csv");
+export const PATH_TO_RAPORT_CSV = path.join(__dirname, "WebsiteReport.csv");
 
 export const prisma = new PrismaClient();
 export const websiteRepo = new WebsiteRepository();
@@ -16,4 +20,6 @@ const logRepository = new LogRepository();
 export const logService = new LogService(logRepository, websiteService);
 export const websiteController = new WebsiteController(websiteService);
 export const logController = new LogController(logService, websiteService);
-export const cron = new Cron(logService, websiteService);
+export const csvService = new CSVService(websiteService, logService);
+export const cron = new Cron(csvService);
+cron.checkWebsites(PATH_TO_CRON_CSV);
