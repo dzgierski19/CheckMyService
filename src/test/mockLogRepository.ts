62f5c20e7236a78642bcd9eab495e57de9134d04
dzgierski19 <dzgierskigit@gmail.com>
2024-03-29 16:34:00 +0100

    code refactor

--- src/test/mockLogRepository.ts
+++ src/test/mockLogRepository.ts
@@ -1,12 +1,9 @@
 import { randomUUID } from "crypto";
-import { prisma } from "../IoC";
 import { ILogRepository } from "../domains/logs/LogRepository";
 import { Log } from "../domains/logs/LogTypes";
 import { WebsiteStatus } from "../domains/websites/WebsiteTypes";
-import { LogService } from "../domains/logs/LogService";
-import { MockWebsiteRepository, webserv } from "./mockWebsiteRepository";
 
-export class mockLogRepository implements ILogRepository {
+export class MockLogRepository implements ILogRepository {
   fakeDB: Log[] = [];
 
   async addLog(
@@ -26,36 +23,20 @@ export class mockLogRepository implements ILogRepository {
   }
 
   async deleteLog(id: string) {
-    this.fakeDB.map((log) => {
-      if (log.id === id && !log.deleted_at) {
-        log.deleted_at = new Date();
+    this.fakeDB.find((element) => {
+      if (!element.deleted_at && element.id === id) {
+        element.deleted_at = new Date();
       }
     });
   }
 
   async getLog(id: string) {
-    return this.fakeDB.find(
-      (element) => element.id === id && !element.deleted_at
-    );
+    return this.fakeDB.find((element) => {
+      if (element.id === id && !element.deleted_at) return element;
+    });
   }
 
   async getAllLogs() {
     return this.fakeDB.filter((element) => !element.deleted_at);
   }
 }
-
-// const mockLog = new mockLogRepository();
-// const mockLogServ = new LogService(mockLog, webserv);
-
-// async function main() {
-//   await webserv.addWebsite("https://www.litecoin.org");
-//   const websiteId = await webserv.getWebsiteId("litecoin.org/");
-//   await mockLogServ.addLogForWebsite(websiteId);
-//   const logs = await mockLogServ.getAllLogs(websiteId);
-//   await mockLogServ.deleteLog(logs[0].id);
-//   const log = await mockLogServ.getLog(logs[0].id);
-//   const logs2 = await mockLogServ.getAllLogs(websiteId);
-//   console.log(logs2);
-// }
-
-// main();
